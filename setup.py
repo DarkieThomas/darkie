7f33506157f11eaba83dcb532cfdc6da64d29f39
DarkieThomas <frostsaibedinov@gmail.com>
2026-05-08 04:50:07 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+data_files=[
+    (
+        'share/ament_index/resource_index/packages',
+        ['resource/' + package_name]
+    ),
+    (
+        'share/' + package_name,
+        ['package.xml']
+    ),
+    (
+        os.path.join('share', package_name, 'launch'),
+        ['launch/display.launch.py']
+    ),
+],
