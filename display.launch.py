eb667303a8bb8a8d64576eaedb968cf8c5c6d133
DarkieThomas <frostsaibedinov@gmail.com>
2026-05-08 05:07:01 +0700

    Update display.launch.py

--- display.launch.py
+++ display.launch.py
@@ -1,44 +1,35 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
-from ament_index_python.packages import get_package_share_directory
-
 import os
 import xacro
+
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
     pkg_path = get_package_share_directory('my_robot')
 
-    xacro_file = os.path.join(
-        pkg_path,
-        'urdf',
-        'my_robot.urdf.xacro'
-    )
+    xacro_file = os.path.join(pkg_path, 'urdf', 'maze_urdf.xacro')
 
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_description = {'robot_description': robot_description_config.toxml()}
+    robot_description = xacro.process_file(xacro_file).toxml()
 
     return LaunchDescription([
 
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[robot_description],
+            parameters=[{'robot_description': robot_description}],
             output='screen'
         ),
 
         Node(
             package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            output='screen'
+            executable='joint_state_publisher_gui'
         ),
 
         Node(
             package='rviz2',
-            executable='rviz2',
-            output='screen'
+            executable='rviz2'
         )
-
     ])
