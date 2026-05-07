292fa8df7862511f610fdd2edec46a9b87bac946
DarkieThomas <frostsaibedinov@gmail.com>
2026-05-08 05:26:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 data_files=[
-    (
-        'share/ament_index/resource_index/packages',
-        ['resource/' + package_name]
-    ),
-    (
-        'share/' + package_name,
-        ['package.xml']
-    ),
-    (
-        os.path.join('share', package_name, 'launch'),
-        ['launch/display.launch.py']
-    ),
-],
+    ('share/ament_index/resource_index/packages',
+     ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch',
+     ['launch/display.launch.py']),
+    ('share/' + package_name + '/urdf',
+     glob('urdf/*')),
+]
